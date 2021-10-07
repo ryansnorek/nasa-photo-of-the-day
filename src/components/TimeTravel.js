a81633c71243db507b3d95e4885bf5523d4297db
Ryan <snorekr@gmail.com>
2021-10-07 15:18:17 -0400

    Added styled-compnoents

--- src/components/TimeTravel.js
+++ src/components/TimeTravel.js
@@ -1,8 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { API_KEY, todaysDate } from '../constants'
+import styled from 'styled-components'
 
-export default function Launch(props) {
+const StyledTimeTravel = styled.div`
+    * {
+        color: #4f4f4;
+        font-family: sans-serif;
+    }
+`
+
+
+
+export default function TimeTravel(props) {
     const { randomDate } = props
 
     const [date, setDate] = useState(todaysDate)
@@ -12,18 +22,21 @@ export default function Launch(props) {
         axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${date}`)
             .then(res => {
                 setNasaData(res.data)
+                console.log(res.data)
             })
             .catch(err => `Error: ${err}`)
     }, [date])
 
     return (
         <div>
-            <h1>NASA</h1>
-            <button onClick={() => setDate(randomDate())}>Time Travel</button>
-            <p>{nasaData.date}</p>
-            <h3>{nasaData.title}</h3>
-            <p>{nasaData.explanation}</p>
-            <img src={nasaData.url} alt={nasaData.title}></img>
+            <StyledTimeTravel>
+                <h1>NASA</h1>
+                <button onClick={() => setDate(randomDate())}>Time Travel</button>
+                <p>{nasaData.date}</p>
+                <h3>{nasaData.title}</h3>
+                <p>{nasaData.explanation}</p>
+                <img src={nasaData.url} alt={nasaData.title}></img>
+            </StyledTimeTravel>
         </div>
     )
 }
